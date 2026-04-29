0285fb929fe40ffe93bb4190be3d848077e1bc89
yadu0124 <yadu12359@gmail.com>
2026-04-29 14:16:16 +0300

    Tap Pay fixes and Checkout page changes

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -1,4 +1,4 @@
 // Alias route for /checkout - re-exports from ($locale).checkout.tsx
-import {loader} from './($locale).checkout';
-export {loader};
+import {loader, action} from './($locale).checkout';
+export {loader, action};
 export {default} from './($locale).checkout';
